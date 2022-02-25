16c0a80948674fb0e225699768af4583b892ed84
Na-aim <fredericksnaaim13@gmail.com>
2022-02-25 08:43:40 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
+
+app.use(express.json())
+
+const usersRouter = require('./app/routes/users.routes')
+app.use('./users', usersRouter)
+const productsRouter = require('./app/routes/products.routes')
+app.use('./products', productsRouter)
+
+
+app.listen(process.env.PORT || 3000, () => console.log('Server Started'))
