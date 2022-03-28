10e72c87ffcbe3432b911522d42499d8d35b9141
Na-aimOverlord <91248779+Na-aim@users.noreply.github.com>
2022-03-28 14:13:40 +0200

    test sign

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+// require("dotenv").config();
 
 const express = require("express");
 const app = express();
@@ -13,10 +13,10 @@ mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
-
+// Naaim testing
 app.use(express.json());
 app.use(cors())
 app.use("/products", productsRouter);
 app.use("/users", usersRouter);
 app.use("/cart", cartRouter);
-app.listen(process.env.PORT || 3000, () => console.log(" Server started @ hi! American-sterns "));
+app.listen(process.env.PORT || 3000, () => console.log(" Server started @ hi!!! American-sterns "));
